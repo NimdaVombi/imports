a48f50e5363ac47c1365406c45c2c347288d8f6d
NimdaVombi <103864968+NimdaVombi@users.noreply.github.com>
2023-01-14 23:18:48 +0000

    Uninstall button

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ class gui:
     [sg.Text("Select an option:")],
     [sg.Button("Option 1"), sg.Button("Option 2")],
     [sg.Button("Option 3"), sg.Button("Option 4")],
-    [sg.Button("Quit")]
+    [sg.Button("Quit"), sg.Button("Uninstall")]
 	]
 
 	window = sg.Window("imports v1", layout)
 
 	while True:
 		event, values = window.read()
-		if event == "Option 1":
-			print('hiiiii')
+		if event == "Uninstall":
+			os.system("sh files/uninstall.sh")
 		if event in (sg.WIN_CLOSED, "Quit"):
 			break
 
